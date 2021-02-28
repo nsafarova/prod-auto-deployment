7fef5343998988412b8eb45eb77109c40e3bd6b9
nsafarova <58469217+nsafarova@users.noreply.github.com>
2021-02-28 18:47:51 +0400

    done

--- src/test/java/com/in28minutes/springboot/tutorial/basics/application/configuration/SpringBootTutorialBasicsConfigurationApplicationTests.java
+++ src/test/java/com/in28minutes/springboot/tutorial/basics/application/configuration/SpringBootTutorialBasicsConfigurationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.springboot;
+package com.in28minutes.springboot.tutorial.basics.application.configuration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StudentServicesApplicationTests {
+public class SpringBootTutorialBasicsConfigurationApplicationTests {
 
 	@Test
 	public void contextLoads() {
